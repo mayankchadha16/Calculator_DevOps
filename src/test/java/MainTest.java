4e1e2bc74492860d627b6a8be1ccbe70af6266cc
mayankchadha16 <chadhamayank1609@gmail.com>
2023-10-21 17:26:00 +0530

    finish test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,8 +10,20 @@ public class MainTest {
     }
 
     @Test
+    public void testFact() {
+        int x = Main.factorialHelper(4);
+        Assert.assertEquals(x, 24);
+    }
+
+    @Test
     public void testLog() {
         Double x = Main.naturalLogHelper(10);
         Assert.assertEquals(x, 2.303, 1e-10);
+    }
+
+    @Test
+    public void testPower() {
+        int x = Main.powerHelper(4, 2);
+        Assert.assertEquals(x, 16);
     }
 }
